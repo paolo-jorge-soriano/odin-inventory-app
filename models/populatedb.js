6cd1f259819a480b6d51b5b622e78aa5f2045965
paolo-jorge-soriano <182842652+paolo-jorge-soriano@users.noreply.github.com>
2025-07-28 15:15:07 +0800

    create populatedb.js script

---
+++ models/populatedb.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const { Client } = require("pg");
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS categories (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS products (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  category_id INTEGER REFERENCES categories(id) ON DELETE SET NULL
+);
+
+INSERT INTO products (name, category_id) VALUES
+  ('DOOM', 1),
+  ('Elden Ring', 2),
+  ('Tetris', 3),
+  ('God of War', 1),
+  ('Final Fantasy VII', 2);
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
