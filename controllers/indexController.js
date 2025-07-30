dd8920b17af7b42119b7db390d332def5c13fee4
paolo-jorge-soriano <182842652+paolo-jorge-soriano@users.noreply.github.com>
2025-07-30 22:10:42 +0800

    add header and hero section

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,12 +1,8 @@
 const db = require("../models/queries");
 
 async function getIndexPage(req, res) {
-  const categories = await db.getAllCategories();
-  const products = await db.getAllProducts();
   res.render("index", {
     title: "Home",
-    categories: categories,
-    products: products,
   });
 }
 
